ff8b89465e0172667e0b4f6aedb497b557534e0c
Alex Nordin <apnordin@gmail.com>
2020-11-23 20:45:28 -0500

    Created tables

---
+++ emanagerDB.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS emanagerDB;
+CREATE DATABASE emanagerDB;
+USE emanagerDB;
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(6,2) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
